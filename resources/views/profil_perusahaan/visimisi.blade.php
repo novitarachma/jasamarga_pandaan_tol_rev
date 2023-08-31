b9a4e209a512d93cb42cdc91e75d7a3ff519739e
AlphaP45 <alphapramadhani45@gmail.com>
2023-08-31 15:30:00 +0700

    visimisi

---
+++ resources/views/profil_perusahaan/visimisi.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.user')
+@section('content')
+<section id="portfolio-details" class="portfolio-details">
+    <div class="container">
+        <div class="section-title">
+            <h2>   </h2>
+            <h2>Visi, Misi, dan Tata Nilai</h2>
+        </div>
+    </div>
+
+</section>
+@endsection
