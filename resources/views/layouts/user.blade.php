0fe80c4c790556f0e8a4ea4373180a65411c7031
AlphaP45 <alphapramadhani45@gmail.com>
2023-11-27 09:28:24 +0700

    sss

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -49,7 +49,9 @@
   <link href="{{ asset('assets/css/susunan.css') }}" rel="stylesheet" />
   <link href="{{ asset('assets/css/direksi.css') }}" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="{{ asset('assets/css/profile.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/Set-profile.css') }}" rel="stylesheet" />
   <link href="{{ asset('assets/css/visi.css') }}" rel="stylesheet" />
   <link href="{{ asset('assets/css/tarif.css') }}" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
