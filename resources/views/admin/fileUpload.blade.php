6280256eeadd75baf10169b0cee08e08ee89f8a2
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-12-04 10:04:03 +0700

    update form upload

--- resources/views/admin/fileUpload.blade.php
+++ resources/views/admin/fileUpload.blade.php
@@ -58,8 +58,13 @@
                             <label for="exampleInputFile">File input</label>
                             <div class="input-group">
                                 <div class="custom-file">
-                                    <input type="file" name="file" class="custom-file-input" id="exampleInputFile">
+                                    <input type="file" name="file"
+                                        class="custom-file-input @error('file') is-invalid @enderror"
+                                        id="exampleInputFile">
                                     <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                    @error('file')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
