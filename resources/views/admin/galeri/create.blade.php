cf25c3b359178259b2b9c074b620e459b0160be6
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-12-06 15:41:21 +0700

    add crud galeri

---
+++ resources/views/admin/galeri/create.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.admin')
+
+@section('style')
+
+@endsection
+
+@section('title')
+<div class="col-sm-6">
+    <h1>Add Galeri</h1>
+</div>
+<div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{ route('admin.page') }}">Home</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('galeri.index') }}">Galeri</a></li>
+        <li class="breadcrumb-item active">Create Galeri</li>
+    </ol>
+</div>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="card card-default">
+        <div class="card-header">
+            <h3 class="card-title">Add Galeri</h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+            </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <form method="post" action="{{ route('galeri.store') }}" id="myForm" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="judul">Judul</label>
+                            <input type="text" class="form-control @error('judul') is-invalid @enderror" id="judul"
+                                name="judul" placeholder="Enter Judul">
+                            @error('judul')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="foto">File Foto</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" name="foto"
+                                        class="custom-file-input @error('foto') is-invalid @enderror" id="foto">
+                                    <label class="custom-file-label" for="foto">Choose file</label>
+                                    @error('foto')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="tahun">Tahun</label>
+                            <select class="form-control select2bs4" style="width: 100%;" id="tahun" name="tahun">
+                                @foreach($tahun as $th)
+                                <option value="{{ $th->id }}" selected="selected">{{ $th->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-12">
+                        <a href="{{ route('galeri.index') }}" class="btn btn-secondary">Cancel</a>
+                        <button type="submit" class="btn btn-success float-right">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+
+@endsection
