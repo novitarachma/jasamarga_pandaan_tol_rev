b2604836b3dae4ab6b4258a11570378735b71639
DekiFirmansyah <dekifirmansyah99@gmail.com>
2023-12-13 09:01:45 +0700

    update controller admin

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -11,21 +11,26 @@ use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\UserImport;
 use App\Imports\TarifTolImport;
 use App\Imports\GajiImport;
+use App\Models\Tahun;
+use App\Models\Bulan;
 
 class UploadController extends Controller
 {
     public  function upload()
     {
-        $labelList = [
+        $label = [
 			'User',
 			'Tarif Tol',
 			'Gaji',
 	    ];
         
-        return view('admin.fileUpload', ['label' => $labelList]);
+        $tahun = Tahun::all();
+        $bulan = Bulan::all();
+        
+        return view('admin.fileUpload', compact(
+            'label', 'tahun', 'bulan'
+        ));
     }
-
-    const FOLDER = './app/public/';
     
     public function fileUpload(ImportFileRequest $request, FileUploadService $fileUploadService)
 	{
@@ -35,20 +40,22 @@ class UploadController extends Controller
         }
         
         $label = $request->get('label');
+        $tahun = $request->get('tahun');
+        $bulan = $request->get('bulan');
         
         // import data
         if($label == 'User'){
-            Excel::import(new UserImport, storage_path(FOLDER.$filePath));
+            Excel::import(new UserImport, storage_path('./app/public/'.$filePath));
         }elseif($label == 'Gaji'){
-            Excel::import(new GajiImport, storage_path(FOLDER.$filePath));
+            Excel::import(new GajiImport($tahun, $bulan), storage_path('./app/public/'.$filePath));
         }elseif($label == 'Tarif Tol'){
-            Excel::import(new TarifTolImport, storage_path(FOLDER.$filePath));
+            Excel::import(new TarifTolImport, storage_path('./app/public/'.$filePath));
         }
 		
 		// notifikasi dengan session
 		Session::flash('sukses','Data User Berhasil Diimport!');
  
 		// alihkan halaman kembali
-		return redirect()->route('fileUpload');
+		return redirect()->route('upload')->with('success', 'Data '. $label .' Berhasil Ditambahkan');;
 	}
 }
